88e648fbafa50e4cc8cbd6c6aee97ce053fe47a9
Erdemina <34918271+Erdemina@users.noreply.github.com>
2021-05-09 14:24:23 +0300

    Update terspiramit.cpp

--- terspiramit.cpp
+++ terspiramit.cpp
@@ -3,15 +3,15 @@ using namespace std;
 int main() {
 	int boyut;
 	cout<<"Boyutu kaç olsun ?\n>>";
-	cin>>boyut;
-	int bosluk=0;
-	for (; boyut>=1; boyut--) {
+	cin>>boyut; //piramidin uzunluğunu seçtiriyoruz.
+	int bosluk=0; 
+	for (; boyut>=1; boyut--) { //girilen boyut kadar satır olacağı için for döngüsüyle boyut kadar satır yazmasını sağlıyoruz. 
 		
-		for (int i=0; i<=bosluk; i++) {
-		     cout<<" ";
+		for (int i=0; i<=bosluk; i++) { // * koymadan önceki boşluklar için bir for döngüsü.
+		     cout<<" "; //
 		}
 		bosluk++;
-	    for(int x=1; x<=boyut; x++) {
+	    for(int x=1; x<=boyut; x++) { // * koymak için gereken for döngüsü.
 			cout<<"*";
 		}
         
