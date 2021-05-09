a8d8ccaadba4a8fc2c405e0cb64ca7cd5bbd6c18
Erdemina <34918271+Erdemina@users.noreply.github.com>
2021-05-09 14:27:46 +0300

    Update ntane_sayi_topla.cpp

--- ntane_sayi_topla.cpp
+++ ntane_sayi_topla.cpp
@@ -3,15 +3,15 @@
 #include <iostream>
 using namespace std;
 int main() {
-    int n, toplam=0,sayi;
+    int n, toplam=0,sayi;//toplamı 0 dan başlatıyoruz çünkü toplamanın etksisiz elemanı 0'dır ve toplanan değerler burada birikeceği için fazladan veya eksik değer almasını istemeyiz.
     cout<<"Bu bir deneme programidir."<<endl;
     cout<<"Sayi adedi kac olsun:"<<endl;
-    cin>>n;
-    for (int i = 1; i<=n; i++)
+    cin>>n; //kaç sayı toplayacğını seçtiriyoruz
+    for (int i = 1; i<=n; i++) //kaç sayı toplayacağını seçmiştik girilen sayı adedi kadar sayı almak için for döngüsü kullanılıyor.
     {
         cout<<i<<". sayiyi giriniz:";
-        cin>>sayi;
-        toplam=toplam+sayi;
+        cin>>sayi; 
+        toplam+=sayi;// toplam=toplam+sayi(toplam+=sayi)  sayıyı topluyor ve değerini toplama atıyor böylece toplam ın içinde değerler birikiyor ve toplama işlemi gerçekleşiyor.
     }
     cout<<"Sonuc="<<toplam<<endl;
     return(0);
